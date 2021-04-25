54b86634839cc6ddf8fbce809b18a8fd625acd54
Xypher <47769094+XypherCodez@users.noreply.github.com>
2021-04-25 11:24:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
-const readline=require('readline')
-const rl=readline.createInterface({input:process.stdin,output:process.stdout,})
-rl.question("How long should the password be? ",function(passLength){console.log('Generating password with '+passLength+" characters...")
-function genPassword(length){var result='';var characters='ABCDEFGHIJKLMNOPQRSTUVWXZYabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*{}()[]+_-';var extra='0123456789!@#$%^&*{}()[]+_-`=/><.,|';var charactersLength=characters.length;for(var i=0;i<length;i++){result+=characters.charAt(Math.floor(Math.random()*charactersLength));if(i%5===0){result+=extra.charAt(Math.floor(Math.random()*extra.length))}}
-return result;}
-let i=genPassword(passLength)
-console.log(i)
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+rl.question("How long should the password be?", function(passLength) {
+    console.log('Generating password with' + passLength + " characters...")
+    function genPassword(length) {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXZYabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*{}()[]+_-';
+        var extra = '0123456789!@#$%^&*{}()[]+_-'; // Every 5 characters one of these will be selected, for top performance.
+        var charactersLength = characters.length;
+  
+        for (var i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            if (i % 5 === 0) {
+                result += extra.charAt(Math.floor(Math.random() * extra.length))
+            }
+        }
+        return result;
+    }
+    let i = genPassword(passLength)
+    console.log(i)
 
-credits = "This is the end of the password but thank you so much for using Simple-Password-Generator, please star and fork the repository at https://github.com/XypherCodez/Simple-Password-Generator it helps out a lot"
-console.log(credits)
+    const credits = "Thank you for using my generato, I hope that you use this password to secure your accounts, and I wish that you star and fork the repository https://github.com/XypherCodez/Simple-Password-Generator"
+    console.log(credits)
 
-return process.exit()})
+    return process.exit()
+})
